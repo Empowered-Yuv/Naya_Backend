89d2988c751b7b4240a4cec2b404e621d4adc781
Empowered-Yuv <yuvkesarwani10@gmail.com>
2024-03-24 19:24:39 +0530

    add new routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.models.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import jwt from "jsonwebtoken"
+import mongoose from "mongoose"
 
 
 
@@ -514,6 +515,63 @@ const getUserChannelProfile = asyncHandler ( async(req, res) => {
 
 } )
 
+//_id mai string milti hai sab mongoose kaam kr deta hai par aggregation pipelines ka kaam direct jaata hai hai 
+const getWatchHistory = asyncHandler( async(req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user?._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullname: 1,
+                                        username: 1,
+                                        avatar: 1,
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        // for front end only
+                        $addFields: {
+                            ownwer: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user[0].watchHistory,
+            "Watch History fetched Successfully"
+        )
+    )
+} )
+
 export { 
     registerUser,
     loginUser,
@@ -524,5 +582,6 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,14 @@
 import { Router } from 'express'
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from '../controllers/user.controller.js'
+import { 
+    changeCurrentPassword,
+    getCurrentUser,
+    getUserChannelProfile,
+    getWatchHistory,
+    loginUser,
+    logoutUser, 
+    refreshAccessToken, 
+    registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage
+ } from '../controllers/user.controller.js'
 
 import { upload } from '../middlewares/multer.middleware.js'
 
@@ -33,5 +42,22 @@ router.route("/logout").post(verifyJWT, logoutUser)
 //router.route("/logout").post(verifyJWT, anothermiddle wares logoutUser)
 
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+
+router.route("/update-coverImage").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+
+
+//params se le rahe hai isliye colon ke baad hai username
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+
+router.route("/history").get(verifyJWT, getWatchHistory)
+
+
 
 export default router
